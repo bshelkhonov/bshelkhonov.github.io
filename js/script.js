27b75de430da2ed52324503d6df94910270b9e54
Bulat Shelkhonov <shelbulat@mail.ru>
2019-07-16 16:44:31 +0300

    fix 16 07

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-document.body.style.backgroundColor = "#" + getRandInt(0, (1 << 24) - 1).toString(16); 
+document.body.style.backgroundColor = "#" + getRandInt(0, 1 << 24).toString(16); 
 
 function randBgColor() {
-  document.body.style.backgroundColor = "#" + getRandInt(0, (1 << 24) - 1).toString(16); 
+  document.body.style.backgroundColor = "#" + getRandInt(0, 1 << 24).toString(16); 
 }
