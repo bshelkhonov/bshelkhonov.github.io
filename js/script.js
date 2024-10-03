f182977250ff6b34350b92a9087f4dc3bfd511f7
Bulat Shelkhonov <shelbulat@mail.ru>
2024-10-03 17:19:17 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
 "use strict";
 
+const initdata = window.Telegram.WebApp.initData;
 
-function getRandInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function decToHex(num) {
-  num = num.toString(16);
-  while (num.length < 6) {
-    num = "0" + num;
-  } 
-  return "#" + num;
-}
-
-
-function getRandomParam() {
-  return "linear-gradient(90deg, " + 
-          decToHex(getRandInt(0, 1 << 24)) + ", " + decToHex(getRandInt(0, 1 << 24)) + ")";
-}
-
-document.body.style.background = getRandomParam();
-
-function randBgColor() {
-  document.body.style.background = getRandomParam();
-}
+window.location.href = `https://shelbulat.getcourse.ru/page0?tg_initdata=${initdata}`;
